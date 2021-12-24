f9b1b3cd6b6cde53984ee1cc223e7e27442a42d6
Ajinkyap22 <palaskarap15@gmail.com>
2021-12-24 20:36:38 +0530

    Added more tests, navbar and hero section for homepage

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0278E4",
+        secondary: "#626262",
+      },
+      spacing: {
+        128: "32rem",
+      },
+    },
+  },
+  plugins: [],
+};
