914d0696dda82d71e33dacf367e6416d61cfd55e
Ajinkyap22 <palaskarap15@gmail.com>
2022-02-20 20:08:40 +0530

    Feature: Addd forum info box on forum page

--- client/src/Components/Forum/Forum.js
+++ client/src/Components/Forum/Forum.js
@@ -5,8 +5,7 @@ import { useState } from "react";
 import axios from "axios";
 import Filter from "../Feed/Filter";
 import Post from "../Post/Post";
-import { Link } from "react-router-dom";
-import LogoCropped from "../LogoCropped";
+import ForumInfo from "./ForumInfo";
 
 function Forum({ forum, title }) {
   const [activeTab, setActiveTab] = useContext(TabContext);
@@ -51,34 +50,7 @@ function Forum({ forum, title }) {
 
         <div className="mt-8">
           {/* forum info */}
-          <div className="bg-white shadow-base max-w-sm pb-2">
-            {/* title */}
-            <div className="w-full bg-primary p-3 py-2">
-              <LogoCropped width="40" />
-              <p className="text-white text-lg inline"> {forum.forumName}</p>
-            </div>
-
-            {/* description */}
-            <p className="text-sm p-4">
-              This is your feed. Posts from all of your forums will be displayed
-              here. Head to the 'Forums' tab to see the posts of a specific
-              forum.
-            </p>
-
-            {/* buttons */}
-            <div className="">
-              <button className="w-1/2 mx-auto block py-1.5 my-5 text-xs md:text-sm 2xl:text-base border border-primary bg-primary text-white rounded-full hover:bg-white hover:text-primary">
-                Create Post
-              </button>
-
-              <Link
-                to="/create-forum"
-                className="w-1/2 mx-auto text-center block py-1.5 my-5 text-xs md:text-sm 2xl:text-base border border-primary bg-white text-primary rounded-full hover:bg-primary hover:text-white"
-              >
-                Leave Forum
-              </Link>
-            </div>
-          </div>
+          <ForumInfo forum={forum} />
         </div>
       </section>
     </main>
---
+++ client/src/Components/Forum/ForumInfo.js
@@ -0,0 +1,69 @@
+import LogoCropped from "../LogoCropped";
+import moment from "moment";
+
+function ForumInfo({ forum }) {
+  return (
+    <div className="bg-white shadow-base max-w-sm pb-2">
+      {/* title */}
+      <div className="w-full bg-primary p-3 py-2">
+        <LogoCropped />
+        <p className="text-white text-lg inline"> {forum.forumName}</p>
+      </div>
+
+      {/* info */}
+      <div className="p-2 border-b border-[#cfcdcd] px-3">
+        <p className="text-xs p-1">
+          <span className="underline underline-offset-1">Website: </span>
+          <a href={forum.website} className="text-primary px-2">
+            {forum.website}
+          </a>
+        </p>
+        <p className="text-xs p-1">
+          <span className="underline underline-offset-1">Email: </span>
+          <a href={forum.website} className="text-primary px-6">
+            {forum.email}
+          </a>
+        </p>
+        <p className="text-xs p-1">
+          <span className="underline underline-offset-1">Address: </span>
+          <span className="px-2">{forum.address}</span>
+        </p>
+      </div>
+
+      {/* stats */}
+      <div className="p-3 border-b border-[#cfcdcd] text-center px-24">
+        <div className="flex justify-center items-center">
+          {/* members */}
+          <div className="flex flex-col items-center px-2">
+            <span>{forum.members.length}</span>
+            <span className="text-sm">Members</span>
+          </div>
+
+          {/* posts */}
+          <div className="flex flex-col items-center px-2">
+            <span>{forum.posts.length}</span>
+            <span className="text-sm">Posts</span>
+          </div>
+        </div>
+
+        {/* created data */}
+        <p className="text-xs pt-3 text-secondary">
+          Created {moment(forum.timestamp).format("LL")}
+        </p>
+      </div>
+
+      {/* buttons */}
+      <div>
+        <button className="mx-auto w-1/2 block p-2 py-1.5 my-5 text-xs md:text-sm 2xl:text-base border border-primary bg-primary text-white rounded-full hover:bg-white hover:text-primary">
+          Create Post
+        </button>
+
+        <button className="mx-auto w-1/2 block text-centr p-2 py-1.5 my-5 text-xs md:text-sm 2xl:text-base border border-primary bg-white text-primary rounded-full hover:bg-primary hover:text-white">
+          Leave Forum
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ForumInfo;
