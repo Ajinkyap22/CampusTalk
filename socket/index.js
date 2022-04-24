6ee4299bc08d491a74f1ebd49cf28d8abf1c432e
Ajinkyap22 <palaskarap15@gmail.com>
2022-04-25 00:38:38 +0530

    Update File Paths

--- socket/index.js
+++ socket/index.js
@@ -1,6 +1,8 @@
-const app = require("express")();
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(3000, {
+  cors: {
+    origin: "*",
+  },
+});
 
 let users = {};
 
