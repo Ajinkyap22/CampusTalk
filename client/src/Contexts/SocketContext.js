907dd9b1845ac164536c0153961f51a583f75518
Ajinkyap22 <palaskarap15@gmail.com>
2022-04-25 00:22:50 +0530

    Update File Paths

--- client/src/Contexts/SocketContext.js
+++ client/src/Contexts/SocketContext.js
@@ -8,7 +8,7 @@ export function SocketProvider({ children }) {
   const [onlineUsers, setOnlineUsers] = useState({});
 
   useEffect(() => {
-    socket.current = io("ws://campustalk-app.herokuapp.com/");
+    socket.current = io("https://campustalk-app.herokuapp.com/");
 
     socket.current.on("users", (users) => {
       setOnlineUsers(users);
